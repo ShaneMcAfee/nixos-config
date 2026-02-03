13e55c7d56d5eb17ca0b632b7d02620ae1d89c74
Shane McAfee <shanemcafee10@gmail.com>
2026-02-03 11:29:14 -0800

    Fix: Repair config and enable hibernation/swap

--- hosts/framework16/configuration.nix
+++ hosts/framework16/configuration.nix
@@ -2,22 +2,35 @@
 
 {
   imports =
-    [ 
+    [
       ./hardware-configuration.nix
       ../../modules/desktop.nix
       ../../modules/core.nix
-      ../../modules/virtualization.nix  # <--- Added
+      ../../modules/virtualization.nix
     ];
-
 
   # --- FRAMEWORK 16 SPECIFICS ---
   # Kernel Params for Ryzen AI 300
-  boot.kernelParams = [ "amdgpu.mes=0" "amdgpu.abmlevel=0" ];
-  
-  # Fix "Backpack Wake"
+  # Added amdgpu.sg_display=0 to fix white/backlight-only screen on wake
+  boot.kernelParams = [ "amdgpu.mes=0" "amdgpu.abmlevel=0" "amdgpu.sg_display=0" ];
+
+  # --- POWER & HIBERNATION ---
+  # Fix for "Black Screen" crashes on low battery/hibernate (32GB Swap)
+  swapDevices = [ { device = "/swapfile"; size = 32 * 1024; } ];
+
+  # Ensure we suspend correctly before hibernating
+  systemd.sleep.extraConfig = ''
+    AllowHibernation=yes
+    AllowHybridSleep=yes
+    SuspendState=mem
+  '';
+
+  # --- INPUT WAKE FIXES ---
+  # Prevent Framework Input Modules (Keyboards/Pads) from waking the laptop
+  # Vendor 32ac = Framework. Disabling wake for all 32ac USB devices.
   services.udev.extraRules = ''
-    SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="32ac", ATTRS{idProduct}=="0012", ATTR{power/wakeup}="disabled"
-    SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="32ac", ATTRS{idProduct}=="0014", ATTR{power/wakeup}="disabled"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="32ac", ATTR{power/wakeup}="disabled"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="32ac", ATTR{driver/usb/power/wakeup}="disabled"
   '';
 
   # Firmware, Fingerprint, & Power
@@ -36,7 +49,7 @@
   users.users.shane = {
     isNormalUser = true;
     description = "Shane McAfee";
-    extraGroups = [ "networkmanager" "wheel" "wireshark" "libvirtd"  ]; 
+    extraGroups = [ "networkmanager" "wheel" "wireshark" "libvirtd" ];
     shell = pkgs.zsh;
     packages = with pkgs; [ kdePackages.kate ];
   };
